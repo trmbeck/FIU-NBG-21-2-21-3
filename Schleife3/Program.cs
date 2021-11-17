a254bee97634d0e9cd56151b5ffba08a8c1d38cc
mbeck-tr <mbeck@lutz-und-grub.de>
2021-11-17 15:05:24 +0100

    SchleifeHochRunter

---
+++ Schleife3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Schleife3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hochzählen und runterzählen in einer Schleife");
+
+            // 1,2,3,4,5,6,7,8,9,10,9,8,7,6,5,4,3,2,1
+
+            int inkrement = +1;
+            Console.Write("\n\nAusgabe: ");
+            for (int i = 1; i > 0; i = i + inkrement)
+            {
+                //if (i == 0) break;
+                if (i == 1 && inkrement == -1)
+                {
+                    Console.Write($"{i}");
+                }
+                else
+                {
+                    Console.Write($"{i},");
+                }
+                if (i == 10) inkrement = -1;
+            }
+            //Console.WriteLine("\b ");
+            Console.ReadKey();
+        }
+    }
+}
